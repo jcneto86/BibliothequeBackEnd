0b86259f048e7c16cda738ae5f1e536554425031
João Carlos Fernandes Neto <wysrar@gmail.com>
2018-08-19 20:43:52 -0400

    v0.1.1

---
+++ src/bibliotheque/FonctionsBibli.java
@@ -0,0 +1,69 @@
+package bibliotheque;
+
+import java.util.LinkedList;
+
+public class FonctionsBibli {
+
+    /* longin */
+    public static void validerUtilisateur() {
+
+    }
+
+    public static boolean validerMotDePasse() {
+        return false;
+    }
+
+    /* fin longin */
+
+    public static void reinitialiserMotDePasse() {
+
+    }
+
+    public static LinkedList<Livre> rechercheLivre() {
+        return null;
+    }
+
+    public static void reserverLivre() {
+
+    }
+
+    public static void faireConsultationSurPlace() {
+
+    }
+
+    public static LinkedList<Utilisateur> rechercheUtilisateur() {
+        return null;
+    }
+
+    public static void creerUtilisateur() {
+
+    }
+
+    public static void creerLivre() {
+
+    }
+
+    public static Livre enregistrerUtilisateur(Utilisateur utilisateur) {
+        return null;
+    }
+
+    public static Livre enregistrerLivre(Livre livre) {
+        return null;
+    }
+
+    public static void modifierUtilisateur() {
+
+    }
+
+    public static void modifierLivre() {
+
+    }
+
+    public static void fairePret() {
+
+    }
+
+    public static void faireRetour() {
+
+    }
+}
--- src/sql/RequisitionsSQL.java
+++ src/sql/RequisitionsSQL.java
@@ -1,4 +1,5 @@
 package sql;
 
 public class RequisitionsSQL {
+
 }
