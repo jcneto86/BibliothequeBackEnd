8de2d224374819e91a56cb2cb1f5fd15d0e15ff5
João Carlos Fernandes Neto <wysrar@gmail.com>
2018-09-02 23:19:59 -0400

    v1.0

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
-import entities.OevreLitteraire;
-import net_biblio.Login;
-import sql.SQLClass;
-
-import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        SQLClass.commit();
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Login window = new Login();
-                    window.frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
 
     }
 }
